f1a258c68465c8f494a5b2f57056f9233964bc2c
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2023-03-27 19:25:20 -0700

    Updates

---
+++ precache-manifest.3be956ff1a65cd58ac7a99c6665ec71d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "482c7064d4efa72bd9d2",
+    "url": "/pathfinder/static/css/main.8c588536.chunk.css"
+  },
+  {
+    "revision": "482c7064d4efa72bd9d2",
+    "url": "/pathfinder/static/js/main.482c7064.chunk.js"
+  },
+  {
+    "revision": "443b51e8a8e3a9893c25",
+    "url": "/pathfinder/static/js/1.443b51e8.chunk.js"
+  },
+  {
+    "revision": "daebc6c281728bea39b8",
+    "url": "/pathfinder/static/js/runtime~main.daebc6c2.js"
+  },
+  {
+    "revision": "c82f16404bc76bbd3938008b22779449",
+    "url": "/pathfinder/index.html"
+  }
+];
